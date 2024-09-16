c2a917537b0af2bec0d95239109f26f2aa8769c9
Nguyenvan11-sel <vnguy189@my.centennialcollege.ca>
2024-09-16 20:56:29 +0000

    Update Home and layout

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,39 +1,54 @@
+import React from "react";
+import { Container, Typography, Button, Box } from "@mui/material";
+import { Link } from "react-router-dom";
 import BBG from "../src/assets/BBG.jfif";
 import hire from "../src/assets/hire.jpg";
-import { Link } from "react-router-dom";
-import "../src/index.css";
 
 export default function Home() {
   return (
-    <>
-      <section id="intro">
-        <div className="introContent">
-          <span className="hello">Hello,</span>
-          <br />
-          <span className="introText">
-            I am <span className="introName">Blessing Ajiboye</span>
-            <img
-              src={BBG}
-              alt="profile"
-              className="bbg"
-              width="750"
-              height="750"
-              align="right"
-            />
-            <br />
+    <Container maxWidth="lg" sx={{ mt: 8 }}>
+      <Box
+        id="intro"
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box sx={{ flexGrow: 1, pr: 4 }}>
+          <Typography variant="h3" component="span" gutterBottom>
+            Hello,
+          </Typography>
+          <Typography variant="h4" component="span">
+            I am <strong>Blessing Ajiboye</strong>
+          </Typography>
+          <Typography variant="h6" component="div" gutterBottom>
             Website Designer
-          </span>
-          <p className="introPara">
+          </Typography>
+          <Typography variant="body1" component="p" gutterBottom>
             I am a skilled web designer with experience in creating visually
-            appealing user friendly websites.
-          </p>
-          <Link>
-            <button className="btn">
-              <img src={hire} alt="Hire me" width="100px" height="100px" />
-            </button>
+            appealing and user-friendly websites.
+          </Typography>
+          <Link to="/contact" style={{ textDecoration: "none" }}>
+            <Button variant="contained" color="primary" size="large">
+              <img
+                src={hire}
+                alt="Hire me"
+                style={{ width: "24px", height: "24px", marginRight: "8px" }}
+              />
+              Hire me
+            </Button>
           </Link>
-        </div>
-      </section>
-    </>
+        </Box>
+        <Box>
+          <img
+            src={BBG}
+            alt="Blessing Ajiboye's profile"
+            style={{ width: "350px", height: "350px", borderRadius: "50%" }}
+          />
+        </Box>
+      </Box>
+    </Container>
   );
 }
--- client/components/layout.jsx
+++ client/components/layout.jsx
@@ -1,25 +1,70 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Link, Box } from "@mui/material";
 import football1 from "../src/assets/football1.jfif";
 
 export default function Layout() {
   return (
     <>
-      <img
-        src={football1}
-        alt="football"
-        className="football"
-        width="50px"
-        height="50px"
-      />
-      <h1>My Portfolio</h1>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
-        <Link to="/education">Education</Link>|{" "}
-        <Link to="/project">Project</Link>| <Link to="/contact">Contact</Link>
-      </nav>
-      <br />
-      <hr />
+      <AppBar position="static">
+        <Toolbar>
+          <Box sx={{ display: 'flex', alignItems: 'center', mr: 2 }}>
+            <img
+              src={football1}
+              alt="football"
+              style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+            />
+          </Box>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            My Portfolio
+          </Typography>
+          <nav>
+            <Link
+              component={RouterLink}
+              to="/"
+              color="inherit"
+              sx={{ mx: 2, textDecoration: "none" }}
+            >
+              Home
+            </Link>
+            <Link
+              component={RouterLink}
+              to="/about"
+              color="inherit"
+              sx={{ mx: 2, textDecoration: "none" }}
+            >
+              About
+            </Link>
+            <Link
+              component={RouterLink}
+              to="/education"
+              color="inherit"
+              sx={{ mx: 2, textDecoration: "none" }}
+            >
+              Education
+            </Link>
+            <Link
+              component={RouterLink}
+              to="/project"
+              color="inherit"
+              sx={{ mx: 2, textDecoration: "none" }}
+            >
+              Project
+            </Link>
+            <Link
+              component={RouterLink}
+              to="/contact"
+              color="inherit"
+              sx={{ mx: 2, textDecoration: "none" }}
+            >
+              Contact
+            </Link>
+          </nav>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ my: 2 }}>
+        <hr />
+      </Box>
     </>
   );
 }
